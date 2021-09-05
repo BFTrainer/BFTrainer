cc06ff90b4ccd1f5174a8caf14eba3a59b508f03
Zhengchun Liu <lzhengchun@users.noreply.github.com>
2021-09-05 10:55:02 -0500

    Update job_submit_example.sh
    revised sample to meet bound req

--- job_submit_example.sh
+++ job_submit_example.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/python
 
+# this sample is wrong, the last item of N should be the same as `max`, similiar the first item should be `min`
 python BFSub.py --min 1 --max 3 --N 1 2 3 4 5 --O 1 1.8 2.6 3.4 4.2 --res_up 3 --res_dw 1 --path train.py
+
+# python BFSub.py --min 1 --max 5 --N 1 2 3 4 5 --O 3500 5500 8500 11000 14000 --res_up 20 --res_dw 7 --path train.py
